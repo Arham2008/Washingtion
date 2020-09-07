3b4f90fd512ed41d4da12e30299613237a5a916f
arham2008 <akbarmunner@gmail.com>
2020-09-06 20:10:14 -0400

    i add my Washingtion project in .git

---
+++ src/art/Painting.java
@@ -0,0 +1,18 @@
+package art;
+
+public class Painting {
+
+	// EscapeSequence:
+	
+	
+			public static void main(String[] args) {
+				
+				
+			    System.out.println("Shaukat \barif");
+				System.out.println("We are learning EscapeSequence");
+				System.out.println("employee name \"akbar Muneer\"");
+			    
+			    
+	
+			}	
+}
